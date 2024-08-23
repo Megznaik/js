a801421e3d6e731ee82b2b96a685826a94ac6a84
Megznaik <159499761+Megznaik@users.noreply.github.com>
2024-08-23 01:45:50 +0000

    variables of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 14452
+let accountEmail = "megha@gmail.com"
+var accPassword = "1234"
+accountCity = "Mumbai"
+let accountState
+//accountId = 2 not allowed
+
+accountEmail = "megz027@gmail.com"
+accPassword = "18236"
+accountCity = "Pune"
+
+console.log(accountId);
+
+console.table([accountId,accPassword,accountEmail,accountCity,accountState])
