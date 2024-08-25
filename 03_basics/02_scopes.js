71e2a5605b1fc1fe653e17159bd9b6db7846d3df
Megznaik <159499761+Megznaik@users.noreply.github.com>
2024-08-25 02:55:56 +0000

    scopes in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "megha"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "megha"
+    if (username === "megha") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // this will not give error
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // this will give error
+const addTwo = function(num){
+    return num + 2
+}
