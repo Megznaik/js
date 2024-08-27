086779f03cfb4bd79fc272d796a6993c48cebc20
Megznaik <159499761+Megznaik@users.noreply.github.com>
2024-08-27 16:06:48 +0000

    Immediately Invoked Function Expressions (IIFE)

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+//---IMP---why we use iife?There is a problem of Global scope pollution most of the time.
+//So, to remove the pollution created by Global scope variable/content, we use iife.
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+( (name) => { //unnamed IIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+} )('megha')
+
+//---IMP---IF asked to write two IIFE then dont forget to put ; after the 1st IIFE function
