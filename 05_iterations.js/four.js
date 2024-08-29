03ab7a14fb86aed8176ad4e113511cfed69d76c5
Megznaik <159499761+Megznaik@users.noreply.github.com>
2024-08-29 16:44:50 +0000

    for-in

---
+++ 05_iterations.js/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+//---IMP-- for-in: in the above case, it will give o/p for console.log(key) as index of array.
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }//----o/p will be : myObject is not iterable--- MAP is not iterable
